e536c01f50392b8458339232ffa8242bfe79ec9d
Humna Liaquat <hanilicious17@gmail.com>
2025-03-13 17:52:14 +0500

    new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 
 import Butoon from "./components/Butoon"
+import Container from "./components/Container"
+
 function App() {
 
   return (
     <>
       <Butoon logo = "Taskely"/>
+      <Container/>
     </>
   )
 }
---
+++ src/components/Container.tsx
@@ -0,0 +1,11 @@
+export default function Container() {
+  return (
+    <div className='main-container'>
+      <div className='activity-container'>
+      </div>
+      <div className='task-statistics'>
+
+      </div>
+    </div>
+  )
+}
